8c0537fbf802aa99497f73e456d4f49e157383f5
Tushar Mehta <tm2582001@gmail.com>
2025-04-26 17:13:06 +0530

    added logic to update profile add shortcut and send notification

---
+++ index.js
@@ -0,0 +1,153 @@
+import puppeteer from "puppeteer-core";
+import createDesktopShortcut from "create-desktop-shortcuts";
+import notifier from "node-notifier";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+
+import config from "./config.json" with { type: "json" };
+const Time = 4 * 60 * 60 * 1000; // milliseconds
+
+const URL = "https://www.naukri.com/mnjuser/profile";
+
+
+function startOnStartup() {
+  let exePath = null;
+  if (process.env.NODE_ENV === "production") {
+    exePath = process.execPath;
+  } else {
+    exePath = __filename;
+  }
+
+  const startupFolder = path.join(
+    process.env.APPDATA,
+    "Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+  );
+
+  const result = createDesktopShortcut({
+    onlyCurrentOS: true,
+    windows: {
+      filePath: exePath,
+      outputPath: startupFolder,
+      name: 'My App Shortcut',
+      comment: 'Auto-created shortcut!',
+      windowMode: 'normal',
+      icon: exePath, // optional
+    }
+  });
+  
+  console.log(result ? 'Shortcut created successfully!' : 'Failed to create shortcut.');
+}
+
+function sendNotification(title, message) {
+  // Send notification
+  notifier.notify({
+    title,
+    message,
+    icon: undefined,
+    sound: true,
+    wait: false,
+    appID: "Profile Updater",
+  });
+}
+
+async function openBrowser() {
+  const browser = await puppeteer.launch({
+    executablePath: config.browserUrl,
+    headless: false,
+  });
+
+  try {
+    const page = await browser.newPage();
+
+    await page.goto(URL, {
+      waitUntil: "networkidle0",
+      timeout: 0,
+    });
+
+    await page.type("#usernameField", config.username);
+    await page.type("#passwordField", config.password);
+    const loginButton = await page.$(
+      ".waves-effect.waves-light.btn-large.btn-block.btn-bold.blue-btn.textTransform"
+    );
+
+    await Promise.all([page.waitForNavigation(), loginButton.click()]);
+
+    await page.goto(URL, {
+      waitUntil: "networkidle0",
+      timeout: 0,
+    });
+
+    //? updating resume
+    // const [fileChooser] = await Promise.all([
+    //   page.waitForFileChooser(),
+    //   page.click("#attachCV"),
+    //   // some button that triggers file selection
+    // ]);
+    // await fileChooser.accept([config.resumeUrl]);
+
+    // //? updating headline
+    // await page.click("#lazyResumeHead .edit.icon");
+
+    // await page.evaluate(() => {
+    //   const inputField = document.querySelector("#resumeHeadlineTxt");
+    //   inputField.value = ""; // Directly set the value to empty
+    // });
+
+    // await page.type(
+    //   "#resumeHeadlineTxt",
+    //   config.resumeHeadlines[Math.round(Math.random() * 1)]
+    // );
+
+    // await page.click(`form[name="resumeHeadlineForm"] .btn-dark-ot`);
+
+    return true;
+  } catch (err) {
+    console.log(err);
+
+    return false;
+  } finally {
+    browser.close();
+  }
+}
+
+async function main() {
+
+  startOnStartup();
+
+  const browserAction = await openBrowser();
+  if (browserAction) {
+    sendNotification(
+      "Profile updated successfully",
+      "Enjoy your life in peace"
+    );
+  } else {
+    sendNotification(
+      "There was some error while updating profile",
+      "Please don't disturb me for your task"
+    );
+  }
+
+  setInterval(async () => {
+    // console.log("Running task at", new Date());
+    const browserAction = await openBrowser();
+
+    if (browserAction) {
+      sendNotification(
+        "Profile updated successfully",
+        "Enjoy your life in peace"
+      );
+    } else {
+      sendNotification(
+        "There was some error while updating profile",
+        "Please don't disturb me for your task"
+      );
+    }
+    // your function or task here
+  }, Time);
+}
+
+(() => {
+  main();
+})();
