edc7d31cddcfb7e7e125fa49156b0ab6a7fb7add
Tushar Mehta <tm2582001@gmail.com>
2025-04-26 22:53:16 +0530

    added flags for easier usage

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
+#!/usr/bin/env node
+
+import * as fsPromise from "fs/promises";
+
 import puppeteer from "puppeteer-core";
 import createDesktopShortcut from "create-desktop-shortcuts";
 import notifier from "node-notifier";
 import { fileURLToPath } from "url";
 import path from "path";
-import {SysTray} from 'node-systray-v2';
-
-// import image from "./profile-changer.ico";
-
-// import imageString from "./image.js";
-
+import { SysTray } from "node-systray-v2";
 import image from "./image.js";
+import arg from "arg";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// import config from "./config.json" with { type: "json" };
+const DEFAULT_UPDATE_TIME = 4 * 60 * 60 * 1000; // milliseconds
+const DEFAULT_BROWSER_PATH =
+  "C:/Program Files/Google/Chrome/Application/chrome.exe";
+
+const URL = "https://www.naukri.com/mnjuser/profile";
+
+const isProduction = typeof process.pkg !== "undefined";
 
 const systray = new SysTray({
   menu: {
     // you should using .png icon in macOS/Linux, but .ico format in windows
     icon: image,
     title: "Profile updater",
-    tooltip: 'Profile updater',
+    tooltip: "Profile updater",
     items: [
       {
-        title: 'Exit',
-        tooltip: 'exit kar oe',
+        title: "Exit",
+        tooltip: "exit kar oe",
         checked: false,
         enabled: true,
       },
@@ -36,18 +47,7 @@ systray.onClick((action) => {
     systray.kill();
     process.exit(0);
   }
-})
-
-const __filename = fileURLToPath(import.meta.url);
-
-import config from "./config.json" with { type: "json" };
-const Time = 4 * 60 * 60 * 1000; // milliseconds
-
-const URL = "https://www.naukri.com/mnjuser/profile";
-
- const isProduction = typeof process.pkg !== 'undefined';
-
-
+});
 
 function startOnStartup() {
   let exePath = null;
@@ -67,14 +67,16 @@ function startOnStartup() {
     windows: {
       filePath: exePath,
       outputPath: startupFolder,
-      name: 'My App Shortcut',
-      comment: 'Auto-created shortcut!',
-      windowMode: 'normal',
+      name: "My App Shortcut",
+      comment: "Auto-created shortcut!",
+      windowMode: "normal",
       icon: exePath, // optional
-    }
+    },
   });
-  
-  console.log(result ? 'Shortcut created successfully!' : 'Failed to create shortcut.');
+
+  console.log(
+    result ? "Shortcut created successfully!" : "Failed to create shortcut."
+  );
 }
 
 function sendNotification(title, message) {
@@ -89,10 +91,10 @@ function sendNotification(title, message) {
   });
 }
 
-async function openBrowser() {
+async function openBrowser(config) {
   const browser = await puppeteer.launch({
     executablePath: config.browserUrl,
-    headless: false,
+    headless: isProduction,
   });
 
   try {
@@ -103,7 +105,7 @@ async function openBrowser() {
       timeout: 0,
     });
 
-    await page.type("#usernameField", config.username);
+    await page.type("#usernameField", config.email);
     await page.type("#passwordField", config.password);
     const loginButton = await page.$(
       ".waves-effect.waves-light.btn-large.btn-block.btn-bold.blue-btn.textTransform"
@@ -117,27 +119,26 @@ async function openBrowser() {
     });
 
     //? updating resume
-    // const [fileChooser] = await Promise.all([
-    //   page.waitForFileChooser(),
-    //   page.click("#attachCV"),
-    //   // some button that triggers file selection
-    // ]);
-    // await fileChooser.accept([config.resumeUrl]);
+    const [fileChooser] = await Promise.all([
+      page.waitForFileChooser(),
+      page.click("#attachCV"),
+      // some button that triggers file selection
+    ]);
+    await fileChooser.accept([config.resumeUrl]);
 
-    // //? updating headline
-    // await page.click("#lazyResumeHead .edit.icon");
-
-    // await page.evaluate(() => {
-    //   const inputField = document.querySelector("#resumeHeadlineTxt");
-    //   inputField.value = ""; // Directly set the value to empty
-    // });
-
-    // await page.type(
-    //   "#resumeHeadlineTxt",
-    //   config.resumeHeadlines[Math.round(Math.random() * 1)]
-    // );
-
-    // await page.click(`form[name="resumeHeadlineForm"] .btn-dark-ot`);
+    if (config.resumeHeadlines?.length) {
+      // //? updating headline
+      await page.click("#lazyResumeHead .edit.icon");
+      await page.evaluate(() => {
+        const inputField = document.querySelector("#resumeHeadlineTxt");
+        inputField.value = ""; // Directly set the value to empty
+      });
+      await page.type(
+        "#resumeHeadlineTxt",
+        config.resumeHeadlines[Math.round(Math.random() *  (config.resumeHeadlines?.length -1))]
+      );
+      await page.click(`form[name="resumeHeadlineForm"] .btn-dark-ot`);
+    }
 
     return true;
   } catch (err) {
@@ -149,11 +150,8 @@ async function openBrowser() {
   }
 }
 
-async function main() {
-
-  startOnStartup();
-
-  const browserAction = await openBrowser();
+async function handleBrowserNotification(config) {
+  const browserAction = await openBrowser(config);
   if (browserAction) {
     sendNotification(
       "Profile updated successfully",
@@ -165,24 +163,150 @@ async function main() {
       "Please don't disturb me for your task"
     );
   }
+}
 
+function usage() {
+  console.log(`tool [CMD]
+  --help\tTo check all the commands
+  --init\tInitialize the app
+  --email String\tadd your email using flag like this '--email example@ex.com'
+  --password String\tadd your password using flag like this '--password yourpassword'
+  --resumeUrl String\tadd your resume path using flag like this ' --resumeUrl "C:/Program Files/Google/Chrome/Application/chrome.exe" ' please notice the '/' and '"'
+  --resumeHeadlines String\tadd your resume path using flag like this ' --resumeHeadlines "Hi am jobless" ' you can use this command multiple times to add multiple headlines code will update one at random
+  --startOnStartup\tenable start on startups only works for windows
+  `);
+}
+
+async function writeConfig(config) {
+  await fsPromise.writeFile(
+    path.resolve(__dirname, "./config.json"),
+    JSON.stringify(config, null, 2)
+  );
+}
+
+async function main() {
+
+  let config = null;
+
+  try {
+    const args = arg({
+      "--init": Boolean,
+      "--help": Boolean,
+      "--email": String,
+      "--password": String,
+      "--resumeUrl": String,
+      "--resumeHeadlines": String,
+      "--startOnStartup": String,
+    });
+
+    // console.log(args);
+    if (args["--help"]) {
+      usage();
+
+      return;
+    } else if (args["--init"]) {
+      await writeConfig({
+        browserUrl: DEFAULT_BROWSER_PATH,
+        timer: DEFAULT_UPDATE_TIME,
+      });
+
+      return;
+    }else if(args["--startOnStartup"]){
+      startOnStartup();
+      return;
+    }
+
+    config = await fsPromise.readFile("./config.json", "utf8").catch((err) => {
+      console.log(`Ohh! configs not found please run this tool with --init flag
+      you can also use --help flag to check all the flags`);
+    });
+
+    if (!config) return;
+
+    config = JSON.parse(config);
+
+    if (!config) return;
+
+    if (args["--email"]) {
+      // console.log(args["--email"]);
+
+      config.email = args["--email"];
+
+      await writeConfig(config);
+      return;
+    }
+
+    if (!config.email) {
+      console.log(
+        `No email found please add your adding flag like '--email example@ex.com'`
+      );
+      return;
+    }
+
+    if (args["--password"]) {
+      // console.log(args["--email"]);
+
+      config.password = args["--password"];
+
+      await writeConfig(config);
+      return;
+    }
+
+    if (!config.password) {
+      console.log(
+        `No password found please add your adding flag like '--password yourpassword'`
+      );
+      return;
+    }
+
+    if (args["--resumeUrl"]) {
+      // console.log(args["--email"]);
+
+      config.resumeUrl = args["--resumeUrl"];
+
+      await writeConfig(config);
+      return;
+    }
+
+    if (!config.resumeUrl) {
+      console.log(
+        `No resume path found please add your adding flag like ' --resumeUrl "C:/Program Files/Google/Chrome/Application/chrome.exe" ' please notice the '/' and '"'`
+      );
+      return;
+    }
+
+    if (args["--resumeHeadlines"]) {
+      // console.log(args["--email"]);
+
+      if (config.resumeHeadlines) {
+        config.resumeHeadlines.push(args["--resumeHeadlines"]);
+      }else{
+        config.resumeHeadlines = [args["--resumeHeadlines"]];
+      }
+
+
+      await writeConfig(config);
+      return;
+    }
+  } catch (err) {
+    if (err.message.includes("Unexpected token")) {
+      console.log(
+        "Looks like there is some error in parsing configs please try running with --init flag again"
+      );
+      return;
+    }
+    console.log(err.message);
+    usage();
+  }
+
+  // console.log("here");
+
+  handleBrowserNotification(config);
   setInterval(async () => {
     // console.log("Running task at", new Date());
-    const browserAction = await openBrowser();
+    handleBrowserNotification(config);
 
-    if (browserAction) {
-      sendNotification(
-        "Profile updated successfully",
-        "Enjoy your life in peace"
-      );
-    } else {
-      sendNotification(
-        "There was some error while updating profile",
-        "Please don't disturb me for your task"
-      );
-    }
-    // your function or task here
-  }, Time);
+  }, config.timer);
 }
 
 (() => {
